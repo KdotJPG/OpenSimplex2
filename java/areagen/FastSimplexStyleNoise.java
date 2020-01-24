d9cc202a409a6292fdb166a22aeddc42fdbfca46
KdotJPG <KdotJPG@users.noreply.github.com>
2020-01-23 20:11:38 -0500

    Update FastSimplexStyleNoise.java

--- java/areagen/FastSimplexStyleNoise.java
+++ java/areagen/FastSimplexStyleNoise.java
@@ -1,5 +1,5 @@
 /**
- * K.jpg's Fast Simplex-Style noise.
+ * K.jpg's Fast Simplex-Style noise ("OpenSimplex (2.0), faster version").
  *
  * - 2D is standard simplex implemented using a lookup table.
  * - 3D is "Re-oriented 4-point BCC noise" which constructs an
